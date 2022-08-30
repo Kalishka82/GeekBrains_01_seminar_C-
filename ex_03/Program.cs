34bfa0cf522e6a0ef4e43ae55653a3efa3c29dd4
Tatiana Kalinina <tankakalinka@gmail.com>
2022-08-30 22:05:40 +0300

    add ex 03

---
+++ ex_03/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+string input = Console.ReadLine(); // пользовательский ввод
+
+int number = int.Parse(input); // если ошибка, программа взрывается
+// if (int.TryParse(input))
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
