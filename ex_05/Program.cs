a3d8f0cc48282ef58230c889edbfc4947c320c4b
Tatiana Kalinina <tankakalinka@gmail.com>
2022-08-30 23:01:14 +0300

    add ex 05

---
+++ ex_05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Input any integer number: ");
+string number = Console.ReadLine();
+int N = int.Parse(number);
+for (int i = -N; i <= N; i++)
+{
+    Console.Write(i);
+    Console.Write(" ");
+}
