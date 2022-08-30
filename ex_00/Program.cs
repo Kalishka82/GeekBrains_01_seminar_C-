0522256a200de766362a40c3352ae9fd4257185e
Tatiana Kalinina <tankakalinka@gmail.com>
2022-08-30 21:43:50 +0300

    add ex 00 initial commit

---
+++ ex_00/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+string input = Console.ReadLine(); // пользовательский ввод
+
+int number = int.Parse(input); // если ошибка, программа взрывается
+// if (int.TryParse(input))
+int square = number * number; // квадрат
+
+Console.WriteLine($"Число в квадрате равно {square}");
